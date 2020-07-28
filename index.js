4345ca9e8d1d83f48bcdda0665cc0c12ba9139b0
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-28 22:20:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+var arr = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  let len = musicians.length;
+  for(let i = 0; i < len; i++) {
+    arr[i] = musicians[i] + " plays " + instruments[i];
+  }
+}
+function johnLennonFacts(facts) {}
+function iLoveTheBeatles(number) {}
