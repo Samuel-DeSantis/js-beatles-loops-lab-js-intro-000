0ab67b13a769588e53bbec3990c20ee8dea3ff5d
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-28 22:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var arr = [];
+
 
 function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
   let len = musicians.length;
   for(let i = 0; i < len; i++) {
     arr[i] = musicians[i] + " plays " + instruments[i];
   }
   return arr;
 }
-function johnLennonFacts(facts) {}
+function johnLennonFacts(facts) {
+  let len = facts.length;
+  for(let i = 0; i < len; i++) {
+    facts[i].
+  }
+}
 function iLoveTheBeatles(number) {}
